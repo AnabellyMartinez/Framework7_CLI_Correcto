1536eabea35d2fc00284572e790d7016fd43410d
lalamartinez <anabely2000ramos@gmail.com>
2021-07-19 11:16:35 -0600

    subir

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,9 +1,12 @@
 
 import InitPage from '../AppPage/init.f7.html';
 import AboutPage from '../AppPage/about.f7.html';
-
-
+//import AgenciaPage from '../AppPage/Agencia.f7.html';
 import NotFoundPage from '../AppPage/404.f7.html';
+import {Listar_Agencia} from '../Controladores/Agencia';
+
+//var Agenca = new Listar_Agencia();
+
 
 var routes = [
   {
@@ -19,6 +22,13 @@ var routes = [
     path: '(.*)',
     component: NotFoundPage,
   },
+  
+  {
+    path: '/Agencia/',
+    beforeEnter: Listar_Agencia(),
+    component: AboutPage,
+  },
+
 
 ];
 
