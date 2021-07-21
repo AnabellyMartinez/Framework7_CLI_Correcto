bfdcc477c8df8e65e8dc6e07e4d8cd867b950456
lalamartinez <anabely2000ramos@gmail.com>
2021-07-21 10:35:41 -0600

    coenxion

---
+++ src/Conexion_BD/Conexion_Framewor7/Usuario/Usuario_Select.php
@@ -0,0 +1,18 @@
+﻿<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Credentials:true");
+header("Access-Control-Allow-Methods: POST,GET,PUT,DELETE,OPTIONS");
+header("Access-Control-Max-Age:604800");
+header("Access-Control-Request-Headers: x-requested-with");
+header("Access-Control-Allow-Headers: x-requested-with, x-requested-by");
+include("../keys.php");
+
+
+$data = array();
+$cari = mysqli_query($conn, "select * from usuario");
+
+while ($row = mysqli_fetch_object($cari)) {
+    $data[] = $row;
+}
+echo json_encode($data);
+
---
+++ src/Conexion_BD/Conexion_Framewor7/keys.php
@@ -0,0 +1,4 @@
+<?php
+ 
+$conn = mysqli_connect("localhost", "root", "1235", "bd_kpis");
+
