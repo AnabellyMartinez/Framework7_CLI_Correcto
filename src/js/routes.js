da2bdc3d901e22335fe30aef8269f7bca7d2da7f
lalamartinez <anabely2000ramos@gmail.com>
2021-07-25 09:06:47 -0600

    subir

--- src/js/routes.js
+++ src/js/routes.js
@@ -8,8 +8,7 @@ import ObjetivosPage from '../AppPage/Objetivos/Objetivos.f7.html';
 import LogradoPage from '../AppPage/Logrado/Logrado.f7.html';
 import MetaPage from '../AppPage/Meta/Meta.f7.html';
 import TiempoPage from '../AppPage/Tiempo/Tiempo.f7.html';
-import TiempoPage from '../AppPage/Tiempo/Tiempo.f7.html';
-import SubobjetivoAreaPage from '../AppPage/Subobjetivo_Area/Subobjetivo_Area.f7.html';
+import SubobjetivoAreaPage from '../AppPage/Subobjetivos_Area/Subobjetivo_Area.f7.html';
 
 
 
